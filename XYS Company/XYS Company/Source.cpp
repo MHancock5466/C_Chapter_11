3c394e4f621f4aa1ff75d3b9391efa5eabbc3d30
Mason <mhancock5466@gmail.com>
2018-09-27 11:06:22 -0500

    Update

---
+++ XYS Company/XYS Company/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int valuesArray[7] = { 50, 10, 5, 25, 30 };
+	int total = 0;
+
+	for (int i = 0; i < size(valuesArray); i++) {
+		cout << valuesArray[i] << endl;
+		total += valuesArray[i];
+	}
+
+	cout << "Total of values: " << total << endl;
+
+	system("pause");
+	return 0;
+}
