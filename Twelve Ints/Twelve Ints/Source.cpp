15a75142dd5c622c98ebd66c2f504d820c1b545f
Mason <mhancock5466@gmail.com>
2018-09-28 10:56:18 -0500

    Update

--- Twelve Ints/Twelve Ints/Source.cpp
+++ Twelve Ints/Twelve Ints/Source.cpp
@@ -2,20 +2,33 @@
 //Author: Mason Hancock
 //Date: 09/28/2018
 #include <iostream>
-#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	int inputs[12] = { 0 };
 
-	for (int i = 0; i < 12; i++) {
+	for (int i = 0; i < size(inputs); i++) {
 		cout << "Input number " << i + 1 << " of 12: ";
 		cin >> inputs[i];
 	}
-
-	for (int i = 0; i < 12; i++) {
+	
+	sort(inputs, inputs + size(inputs));
+	cout << "\nYour Numbers in Ascending Order:" << endl;
+	
+	for (int i = 0; i < size(inputs); i++) {
+		
 		cout << inputs[i] << endl;
 	}
+
+	sort(inputs, inputs + size(inputs), greater<int>());
+	cout << "\nYour Numbers in Descending Order: " << endl;
+	
+	for (int i = 0; i < size(inputs); i++) {
+		
+		cout << inputs[i] << endl;
+	}
+
 	system("pause");
 	return 0;
 }
